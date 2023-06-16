4cb2d4c96fc573f482463a5294fe967f52539607
amit0117 <20je0117@cve.iitism.ac.in>
2023-06-16 19:20:42 +0530

    intial commit

--- public/javascript/clusterMap.js
+++ public/javascript/clusterMap.js
@@ -13,7 +13,7 @@
 		// Choose from Mapbox's core styles, or make your own style with Mapbox Studio
 		style: 'mapbox://styles/mapbox/light-v11',
 		center: [77.947998,23.473324],
-		zoom: 4
+		zoom: 4.5
 	});
 	map.addControl(new mapboxgl.NavigationControl());
 	
